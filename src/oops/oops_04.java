110eded43fcdb2e4f2197f5656a766be3bde519c
Tanmoy05 <tanmoymukherjee746@gmail.com>
2025-09-23 18:23:43 +0530

    oops class and inheritance added

--- src/oops/oops_04.java
+++ src/oops/oops_04.java
@@ -1,4 +1,8 @@
 package oops;
 
 public class oops_04 {
+    public static void main(String[] args){
+
+
+    }
 }
---
+++ src/pattern/patternprin.java
@@ -0,0 +1,15 @@
+package pattern;
+
+public class patternprin {
+    public static void main(String[] args){
+        patternprin.pattern1(5);
+
+    }
+
+    static void pattern1(int n){
+        for(int i =0;i<n;i++){
+            System.out.print("*");
+        }
+
+    }
+}
