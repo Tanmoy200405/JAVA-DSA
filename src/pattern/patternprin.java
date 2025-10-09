dceda01b042906ba0645093668b29040aff38fcb
Tanmoy05 <tanmoymukherjee746@gmail.com>
2025-10-09 22:13:25 +0530

    oops class and inheritance added

--- src/pattern/patternprin.java
+++ src/pattern/patternprin.java
@@ -2,7 +2,7 @@ package pattern;
 
 public class patternprin {
     public static void main(String[] args) {
-        pattern31(4);
+        pattern5(4);
     }
 
     static void pattern31(int n) {
@@ -72,22 +72,20 @@ public class patternprin {
     }
 
     static void pattern5(int n) {
-        for (int row = 0; row < 2 * n; row++) {
-            int totalColsInRow = row > n ? 2 * n - row: row;
-            for (int col = 0; col < totalColsInRow; col++) {
-                System.out.print("* ");
+        for(int row = 1;row<=2*n -1;row++){
+            int tc =
+            for(int col = 1;col<=tc;col++){
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 
     static void pattern4(int n) {
-        for (int row = 1; row <= n; row++) {
-            // for every row, run the col
-            for (int col = 1; col <= row; col++) {
-                System.out.print(col + " ");
+        for(int row = 1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(col);
             }
-            // when one row is printed, we need to add a newline
             System.out.println();
         }
     }
@@ -104,14 +102,12 @@ public class patternprin {
     }
 
     static void pattern1(int n) {
-        for (int row = 1; row <= n; row++) {
-            // for every row, run the col
-            for (int col = 1; col <= n; col++) {
-                System.out.print("* ");
-            }
-            // when one row is printed, we need to add a newline
-            System.out.println();
-        }
+       for(int row=1;row<=n;row++){
+          for(int col = 1;col<=row;col++){
+              System.out.print("*");
+          }
+          System.out.println();
+       }
     }
 
     static void pattern2(int n) {
