d768a3d7cf51b0f0ed2d4f88d29ad00f3d693733
Tanmoy05 <tanmoymukherjee746@gmail.com>
2025-10-24 11:22:55 +0530

    sorting started

---
+++ src/Searching/Ceiling.java
@@ -0,0 +1,30 @@
+package Searching;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+        int ans = ceiling(arr, target);
+        System.out.println(ans);
+    }
+    static int ceiling(int[] arr,int target){
+        if(target> arr[arr.length - 1]){
+            return -1;
+        }
+        int s  = 0;
+        int end = arr.length - 1;
+
+        while(s<=end){
+            int mid = s + (end-s)/2;
+            if(target<arr[mid]){
+                end = mid-1;
+
+            }else if(target>arr[mid]){
+              s = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return s;
+    }
+}
---
+++ src/basic/Reverse.java
@@ -0,0 +1,17 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int ans = 0;
+        while(n>0){
+            int rem = n %10;
+            n /= 10;
+            ans = n*10 + rem;
+        }
+        System.out.println(ans);
+    }
+}
